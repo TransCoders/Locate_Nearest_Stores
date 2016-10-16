6f15552e4d4467b61ad97b809c7f819477b2f911
Alex <Alexandros_Chatzis@yahoo.com>
2016-10-16 13:49:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/alex/mapcurrentlocation/MapsActivity.java
@@ -0,0 +1,268 @@
+package com.example.alex.mapcurrentlocation;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.location.Location;
+import android.location.LocationListener;
+
+import android.util.Log;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    double nlat;
+    double nlng;
+    double glat;
+    double glng;
+
+    LocationManager glocManager;
+    LocationListener glocListener;
+    LocationManager nlocManager;
+    LocationListener nlocListener;
+    TextView textViewNetLat;
+    TextView textViewNetLng;
+    TextView textViewGpsLat;
+    TextView textViewGpsLng;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //All textView
+        textViewNetLat = (TextView) findViewById(R.id.textViewNetLat);
+        textViewNetLng = (TextView) findViewById(R.id.textViewNetLng);
+        textViewGpsLat = (TextView) findViewById(R.id.textViewGpsLat);
+        textViewGpsLng = (TextView) findViewById(R.id.textViewGpsLng);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Maps Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+
+    public class MyLocationListenerNetWork implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+            nlat = loc.getLatitude();
+            nlng = loc.getLongitude();
+
+            //Setting the Network Lat, Lng into the textView
+            textViewNetLat.setText("Network Latitude:  " + nlat);
+            textViewNetLng.setText("Network Longitude:  " + nlng);
+
+            Log.d("LAT & LNG Network:", nlat + " " + nlng);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d("LOG", "Network is OFF!");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d("LOG", "Thanks for enabling Network !");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    }
+
+    //This is for Lat lng which is determine by your device GPS
+    public class MyLocationListenerGPS implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+            glat = loc.getLatitude();
+            glng = loc.getLongitude();
+
+            //Setting the GPS Lat, Lng into the textView
+            textViewGpsLat.setText("GPS Latitude:  " + glat);
+            textViewGpsLng.setText("GPS Longitude:  " + glng);
+
+            Log.d("LAT & LNG GPS:", glat + " " + glng);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d("LOG", "GPS is OFF!");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d("LOG", "Thanks for enabling GPS !");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    }
+
+
+    public void showLoc(View v) {
+
+        //Location access ON or OFF checking
+        ContentResolver contentResolver = getBaseContext().getContentResolver();
+        boolean gpsStatus = Settings.Secure.isLocationProviderEnabled(contentResolver, LocationManager.GPS_PROVIDER);
+        boolean networkWifiStatus = Settings.Secure.isLocationProviderEnabled(contentResolver, LocationManager.NETWORK_PROVIDER);
+
+        //If GPS and Network location is not accessible show an alert and ask user to enable both
+        if (!gpsStatus || !networkWifiStatus) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MapsActivity.this);
+
+            alertDialog.setTitle("Make your location accessible ...");
+            alertDialog.setMessage("Your Location is not accessible to us.To give attendance you have to enable it.");
+            alertDialog.setIcon(R.drawable.cast_ic_notification_small_icon);
+
+            alertDialog.setNegativeButton("Enable", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0);
+                }
+            });
+
+            alertDialog.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(), "Remember to give attandance you have to eanable it !", Toast.LENGTH_SHORT).show();
+                    dialog.cancel();
+                }
+            });
+
+            alertDialog.show();
+        }
+        //IF GPS and Network location is accessible
+        else {
+            nlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            nlocListener = new MyLocationListenerNetWork();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            nlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    1000 * 1,  // 1 Sec
+                    0,         // 0 meter
+                    nlocListener);
+
+
+            glocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            glocListener = new MyLocationListenerGPS();
+            glocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000 * 1,  // 1 Sec
+                    0,         // 0 meter
+                    glocListener);
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng Thessaloniki = new LatLng(40.6436100, 22.9308600);
+        mMap.addMarker(new MarkerOptions().position(Thessaloniki).title("Marker in Thessaloniki"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Thessaloniki, 5));
+
+        LatLng Serres = new LatLng(41.0849900, 23.5475700);
+        mMap.addMarker(new MarkerOptions().position(Serres).title("Marker in Serres"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Serres, 5));
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+}
