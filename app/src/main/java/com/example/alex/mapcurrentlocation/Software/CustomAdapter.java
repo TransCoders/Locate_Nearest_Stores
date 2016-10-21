99c51b874a5e80612934f6234d9d1eb3d8820822
JamesNikolaidis <dimitriosnikolaidis@hotmail.com>
2016-10-21 13:14:21 +0300

    initial commit(James)

--- app/src/main/java/com/example/alex/mapcurrentlocation/Software/CustomAdapter.java
+++ app/src/main/java/com/example/alex/mapcurrentlocation/Software/CustomAdapter.java
@@ -1,9 +1,14 @@
 package com.example.alex.mapcurrentlocation.Software;
 
 import android.content.Context;
+import android.support.v4.widget.TextViewCompat;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.alex.mapcurrentlocation.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +24,15 @@ public class CustomAdapter extends ArrayAdapter {
     }
 
 
+
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
-
-
-
-        
-        return super.getView(position, convertView, parent);
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+            View adapterView = layoutInflater.inflate(R.layout.print_protocol_layout,parent,false);
+            String store = (String) getItem(position);
+            TextView Store_view = (TextView)adapterView.findViewById(R.id.Stores);
+            Store_view.setText(store);
+            return adapterView;
     }
 }
--- app/src/main/java/com/example/alex/mapcurrentlocation/Software/General_Functions.java
+++ app/src/main/java/com/example/alex/mapcurrentlocation/Software/General_Functions.java
@@ -1,6 +1,10 @@
 package com.example.alex.mapcurrentlocation.Software;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+import com.example.alex.mapcurrentlocation.Database.DatabaseFunctions;
 
 import java.util.ArrayList;
 
@@ -12,7 +16,7 @@ public class General_Functions {
     private static float  lon,lat ;
     private  static  Context APPLICATION_CONTEXT;
     private static int THRESHOLD_DISTANCE = 2; // IN KM
-
+    private DatabaseFunctions databaseFunctions ;
 
     public void GeneralFunctions(Context context){
         APPLICATION_CONTEXT=context;
@@ -24,13 +28,49 @@ public class General_Functions {
         lat = coordinates[1];
     }
 
-    public ArrayList<Float> CompareLon_And_Lat(){
+    public ArrayList<String> CompareLon_And_Lat(){
         //COMPARES USERS LAT,LON WITH THE STORES LAN-LON COORDINATES INSIDE THE DATABASE , AND RETURN A ARRAY LIST WITH THE VALUES OF
         //THE SHOPS THAT ARE WITHIN THE PERMITTED THRESHOLD DISTANCE.
+        databaseFunctions = DatabaseFunctions.GetDatabaseObject(APPLICATION_CONTEXT);
+        ArrayList<String>Passes_Stores = new ArrayList<>();
+
+        if(databaseFunctions!=null){
+            String[] Database_Lon_Lan = new String[2];
+            Cursor database_data = databaseFunctions.GetDatabase_ALL_DATA();
+            database_data.moveToFirst();
+            int counter2 = 1;
+            for(int counter1=0; counter1!=database_data.getColumnCount();counter1++,counter2++){
+
+                if(lon-Float.parseFloat(database_data.getString(database_data.getColumnIndex("LONGTITUDE")))<THRESHOLD_DISTANCE && lat-Float.parseFloat(database_data.getString(database_data.getColumnIndex("LATITUDE")))<THRESHOLD_DISTANCE ){
+
+                    try {
+                        Passes_Stores.add(database_data.getString(database_data.getColumnIndex("ONOMA")));
+                        Passes_Stores.add(database_data.getString(database_data.getColumnIndex("DIEYTHINSI")));
+                        Passes_Stores.add(database_data.getString(database_data.getColumnIndex("LONGTITUDE")));
+                        Passes_Stores.add(database_data.getString(database_data.getColumnIndex("LATITUDE")));
+                        Log.d("System_Message", "Insert was Succesfull");
+                    }catch (Exception ex){
+
+                        Log.d("System_Message", "Insert was UnSuccesfull");
+                    }
+
+
+                }else{database_data.moveToNext();}
+
+
+                database_data.moveToNext();
+            }
 
 
 
-       return null;
+
+
+        }
+
+
+
+            return null;
+
     }
 
 
